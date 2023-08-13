89b7a594cf52dd6083bac6c2a47aadd8b3984007
minis <minis>
2023-08-13 11:17:11 +0800

    oo

--- encrypt.js
+++ encrypt.js
@@ -8,9 +8,21 @@ var aes_encrypt = (text, key, iv) =>{
 }
 exports.getencryptedpass = ()=>{
 	const passencrypted = crypto.publicEncrypt(key.pubKey, Buffer.from(key.aeskey+key.aesiv));
-	return passencrypted;
+	return passencrypted.toString('base64');
 }
 exports.encrypt = (data) => {
  const aesencrypted = aes_encrypt(data,key.aeskey,key.aesiv);
  return aesencrypted;
 };
+//以下是解密函数的示例，本项目并不使用，用于服务端解密之参考
+exports.decrypt = (encryptedText, key, iv) => {
+  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+exports.decryptWithPrivateKey = (privateKey, encryptedText) => {
+  const buffer = Buffer.from(encryptedText, 'base64');
+  const decrypted = crypto.privateDecrypt(privateKey, buffer);
+  return decrypted.toString('utf8');
+}
--- send.js
+++ send.js
@@ -1,14 +1,44 @@
 var { encrypt , getencryptedpass } = require('./encrypt');
+const http = require('http');
+const https = require('https');
 
+// 获取环境变量 sendtermuxapiurl 的值
+const termuxapiurl = process.env.sendtermuxapiurl;
+const apiurl = termuxapiurl || 'https://api.example.com/post-endpoint';
+const headers = {
+  'Content-Type': 'application/json',
+  'DecryptPass': getencryptedpass(),
+  'Authorization': 'Bearer YOUR_ACCESS_TOKEN'
+};
+
+const sendPostRequest = (url, headers, body,callback)=>{
+    const options = {
+      method: 'POST',
+      headers: headers
+    };
+
+    const protocol = url.startsWith('https://') ? https : http;
+    const request = protocol.request(url, options, response => {
+      let responseData = '';
+      response.on('data', chunk => {
+        responseData += chunk;
+      });
+      response.on('end', () => {
+	callback(responseData);
+      });
+    });
+    if (body) {
+      request.write(body);
+    }
+    request.end();
+
+}
 
 const sendtoserver = (info)=>{
   const encrypted = encrypt(info); // 加密
-  const encryptedaespass = getencryptedpass();
+  const body = JSON.stringify({ info: encrypted });
   console.log('send to server');
-  console.log(encrypted);
-  console.log(encryptedaespass);
-
-
+  sendPostRequest(apiurl,headers,body,(res)=>{console.log(res);});
 }
 
 
